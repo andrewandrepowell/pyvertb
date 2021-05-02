8a9a7a87c551a3ab61bf1e2f70fe0c69ccfd4300
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-05-01 22:21:34 -0500

    WIP

--- src/pyvert/types.py
+++ src/pyvert/types.py
@@ -1,60 +1,6 @@
 from dataclasses import dataclass, field
 
 
-from cocotb.handle import (
-    SimHandleBase,
-    HierarchyObject,
-    HierarchyArrayObject,
-    NonHierarchyIndexableObject,
-    ModifiableObject,
-    IntegerObject,
-    EnumObject,
-    StringObject,
-    RealObject,
-)
-
-
-Object = SimHandleBase
-"""
-"""
-
-Scope = HierarchyObject
-"""
-"""
-
-ScopeArray = HierarchyArrayObject
-"""
-"""
-
-Logic = ModifiableObject
-"""
-"""
-
-Array = NonHierarchyIndexableObject
-"""
-"""
-
-Record = HierarchyObject
-"""
-"""
-
-Integer = IntegerObject
-"""
-"""
-
-Enum = EnumObject
-"""
-"""
-
-String = StringObject
-"""
-"""
-
-Real = RealObject
-"""
-"""
-
-
 class _RecordMetaclass(type):
     def __new__(typ, name, bases, dict):
         cls = super().__new__(typ, name, bases, dict)
