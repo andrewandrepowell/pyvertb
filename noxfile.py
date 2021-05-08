808f0f6303f241e148a6ba6973dc7e4713ae2b6e
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-05-08 18:24:32 -0500

    Change to RTD theme

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,9 @@ import nox
 
 @nox.session
 def docs(session):
-    session.install("sphinx")
+    # docs dependencies
+    session.install("sphinx", "sphinx-rtd-theme")
+    # self
     session.install(".")
+    # build docs
     session.run("sphinx-build", "-b", "html", "docs/", "html/")
